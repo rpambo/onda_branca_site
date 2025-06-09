e2315ffbb6c1d64d65623897f94d86ddf7e48d5a
Rafael Pambo <rafaelgeniokitoco@gmail.com>
2025-06-09 08:13:20 -0700

    about components, and slideshow responsive

--- web/Onda_branca/src/app/components/home/home.ts
+++ web/Onda_branca/src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../navbar/navbar";
 import { Slides } from "../slides/slides";
+import { About } from "../../layout/about/about";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Slides],
+  imports: [Navbar, Slides, About],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
