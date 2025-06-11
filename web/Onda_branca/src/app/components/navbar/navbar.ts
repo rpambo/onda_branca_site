24f5e94d646e5bead427390a3d814c690f66fd1c
Rafael Pambo <rafaelgeniokitoco@gmail.com>
2025-06-11 07:51:48 -0700

    header chance and add skiti

--- web/Onda_branca/src/app/components/navbar/navbar.ts
+++ web/Onda_branca/src/app/components/navbar/navbar.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  scrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.scrolled = window.scrollY > 50;
+  }
 }
