d9a904fc200b4066b2cb91e3d2b350bc8d969ba1
Rafael Pambo <rafaelgeniokitoco@gmail.com>
2025-06-20 09:29:33 +0100

    add events e modify the mobile

--- web/Onda_branca/src/app/layout/publicacao/publicacao.ts
+++ web/Onda_branca/src/app/layout/publicacao/publicacao.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-publicacao',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './publicacao.html',
   styleUrl: './publicacao.css'
 })
-export class Publicacao {
+export class Publicacao implements AfterViewInit {
 
+  async ngAfterViewInit() {
+    if (typeof window !== 'undefined') {
+      const flatpickr = (await import('flatpickr')).default;
+      flatpickr('#datepicker', {
+        dateFormat:'d-m-y',
+        minDate:'today',
+        disable:[
+          function(date){
+            return (date.getDay() == 0 || date.getDay() == 6);
+          }
+        ]
+      });
+    }
+  }
 }
